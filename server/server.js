eca6239d87d783aa2e1a1d4bbda0241d6a8dcd19
drlst7777 <andreas.landes1989@gmail.com>
2024-02-12 15:05:13 +0100

    backup folder /server + server.js incl. express, cors, & mongoose initialized, local port 8000 for packend server

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/message', (req, res) => {
+    res.json({ message: "Hello from server!" });
+});
+
+
+app.listen(8000, () => {
+    console.log(`Server is running on port 8000.`);
+});
