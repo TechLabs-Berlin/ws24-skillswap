fb01f52803960c6e2641e0a9d995ae21d18293e5
drlst7777 <andreas.landes1989@gmail.com>
2024-02-29 13:13:23 +0100

    bugifxes for mongodb connection

--- server/db.js
+++ server/db.js
@@ -1,8 +1,9 @@
-const Mongoose = require('mongoose');
+const mongoose = require('mongoose');
 //const localDB = `mongodb://localhost:27017/role_auth`;
 const connectDB = async () => {
+    console.log(process.env.MONGODB_URI);
     try {
-        await mongoose.connect(process.env.MONGODB_URI);
+        await mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
         console.log('MongoDB connected');
     } catch (err) {
         console.error('Error connecting to MongoDB', err.message);
--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-require('dotenv').config();
+require('dotenv').config({ path: './mongodb_access.env' });
 const connectDB = require('./db');
 const cookieParser = require('cookie-parser');
 
