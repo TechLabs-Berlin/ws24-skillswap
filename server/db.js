20e7dd6967f544bdf2d322d340295ef5c4ed4233
drlst7777 <andreas.landes1989@gmail.com>
2024-02-29 12:52:33 +0100

    setup online MongoDB database connection

--- server/db.js
+++ server/db.js
@@ -1,10 +1,12 @@
 const Mongoose = require('mongoose');
-const localDB = `mongodb://localhost:27017/role_auth`;
+//const localDB = `mongodb://localhost:27017/role_auth`;
 const connectDB = async () => {
-    await Mongoose.connect(localDB, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
-    console.log('MongoDB Connected');
-}
+    try {
+        await mongoose.connect(process.env.MONGODB_URI);
+        console.log('MongoDB connected');
+    } catch (err) {
+        console.error('Error connecting to MongoDB', err.message);
+        process.exit(1); // Exit application with error
+    }
+};
 module.exports = connectDB;
