dc5a81c1b0f2f0a845104ae6cac636d35fa095bb
4atatime <lexieyu42yce@gmail.com>
2024-03-12 15:49:44 +0100

    connect server test 1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './App.css';
-import { Login } from './Login';
-import { Register } from './Register';
+import { Login } from './form/login/Login';
+import { Register } from './form/registration/Register';
 
 function App() {
   const [currentForm, setCurrentForm] = useState('login');
