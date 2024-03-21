7e57272dac8005b5c80457d5a3e9ac751a2bb721
drlst7777 <andreas.landes1989@gmail.com>
2024-03-21 19:09:49 +0100

    quick fix

--- server/models/user.js
+++ server/models/user.js
@@ -35,32 +35,32 @@ const UserSchema = new Schema({
     },
     description: String,
     isAvailable: Boolean,
-    swapPreference: {
-        type: [{
-            type: String,
-            enum: ['in-person', 'online', 'hybrid']
-        }],
-        validate: [arrayLimit(3), '{PATH} exceeds the limit of 3'] //can select all 3
-    },
-    langPreference: {
-        type: [{
-            type: String,
-            enum: ['English', 'French', 'German', 'Spanish', 'Arabic', 'Mandarin']
-        }],
-        validate: [arrayLimit(6), '{PATH} exceeds the limit of 6'] // can select all 6
-    },
-    location: {
-        type: {
-            type: String,
-            enum: ['Point'],
-            required: true // Only required if 'location' is provided
-        },
-        coordinates: {
-            type: [Number], // example: [-73.856077, 40.848447]
-            required: true // Only required if 'location' is provided
-        }
-        // 'location' itself is optional by not being marked as required
-    },
+    /* swapPreference: {
+         type: [{
+             type: String,
+             enum: ['in-person', 'online', 'hybrid']
+         }],
+         validate: [arrayLimit(3), '{PATH} exceeds the limit of 3'] //can select all 3
+     },
+     langPreference: {
+         type: [{
+             type: String,
+             enum: ['English', 'French', 'German', 'Spanish', 'Arabic', 'Mandarin']
+         }],
+         validate: [arrayLimit(6), '{PATH} exceeds the limit of 6'] // can select all 6
+     },
+     location: {
+         type: {
+             type: String,
+             enum: ['Point'],
+             required: true // Only required if 'location' is provided
+         },
+         coordinates: {
+             type: [Number], // example: [-73.856077, 40.848447]
+             required: true // Only required if 'location' is provided
+         }
+         // 'location' itself is optional by not being marked as required
+     },*/
     skillsOffered: [{
         type: Schema.Types.ObjectId,
         ref: 'Skill' // ID of skills model --> array of skills (_id) the user offers to teach
