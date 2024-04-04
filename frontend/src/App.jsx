17650b5a77ae539981e77cb7337901d21ec85f40
4atatime <lexieyu42yce@gmail.com>
2024-04-04 18:16:43 +0200

    added splash

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,62 @@
-import { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import './components/atoms/Colors.css';
 import { Login } from './form/login/Login';
 import { Register } from './form/registration/Register';
+import SplashScreen from './components/SplashScreen';
 
 function App() {
-  const [currentForm, setCurrentForm] = useState('login');
+  const [loading, setLoading] = useState(true);
 
-  const toggleForm = (formName) => {
-    setCurrentForm(formName);
+  useEffect(() => {
+    // Simulate loading time (e.g., fetch initial data)
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000); // Adjust the time as needed
+  }, []);
+
+  if (loading) {
+    return <SplashScreen />;
   }
 
   return (
     <Router>
       <Switch>
         <Route path="/login" component={Login} />
-        <Route path="/onboarding" component={Onboarding} />
+        <Route path="/register" component={Register} />
         {/* Other routes */}
       </Switch>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
+
+
+// import { useState } from 'react';
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// import './App.css';
+// import './components/atoms/Colors.css';
+// import { Login } from './form/login/Login';
+// import { Register } from './form/registration/Register';
+
+// function App() {
+//   const [currentForm, setCurrentForm] = useState('login');
+
+//   const toggleForm = (formName) => {
+//     setCurrentForm(formName);
+//   }
+
+//   return (
+//     <Router>
+//       <Switch>
+//         <Route path="/login" component={Login} />
+//         <Route path="/onboarding" component={Onboarding} />
+//         {/* Other routes */}
+//       </Switch>
+//     </Router>
+//   )
+// }
+
+// export default App
---
+++ frontend/src/pages/SplashScreen/SplashScreen.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './SplashScreen.css';
+
+const SplashScreen = () => {
+  return (
+    <div className="splash-screen">
+      {/* Content of the splash screen */}
+      <h1>Loading...</h1>
+    </div>
+  );
+};
+
+export default SplashScreen;
