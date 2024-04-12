4176d2fa9b2659c39927fdc904411313001be410
01-johannes <johannes.schaum365@gmail.com>
2024-04-12 11:59:32 +0200

    set up flask

--- DS/02_Application_Code/01_Activity_Status_Flask/Activity_Status_app.py
+++ DS/02_Application_Code/01_Activity_Status_Flask/Activity_Status_app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, jsonify
 import pickle 
 app = Flask(__name__)
 
-with open('C:\Users\Johannes\Documents\Uni\Master\Tech_Labs\ws24-skillswap\DS\01_Notebooks\04_Modeling\Activity_Status_Model\Activity_Status_Model.pkl', 'rb') as file:
+with open(r'C:\Users\Johannes\Documents\Uni\Master\Tech_Labs\ws24-skillswap\DS\02_Application_Code\01_Activity_Status_Flask\Activity_Projection_Model.pkl', 'rb') as file:
     loaded_model = pickle.load(file)
     
 @app.route('/')
@@ -11,11 +11,31 @@ def index():
 
 @app.route('/predict', methods=['Post'])
 def predict():
-    Last_Seen = float(request.form['Last_Seen'])
-    Response_Time = float(request.form['Response_Time'])
-    Above_Average_Track_record = float(request.form['Sum_Skills_Off_ab_avg'])
-    prediction = loaded_model.predict([[Last_Seen, Response_Time, Above_Average_Track_record]])
+    skill_domains = [
+        "Agriculture",
+        "Animal Care",
+        "Art",
+        "Beauty",
+        "Dance",
+        "Music",
+        "Writing",
+        "Language",
+        "Health",
+        "Sports",
+        "Food",
+        "Technology",
+        "Photo",
+        "Video"
+    ]
+
+    input_values = []
+    for skill_domain in skill_domains:
+        input_value = float(request.form.get(skill_domain, 0))
+        input_values.append(input_value)
+
+    prediction = loaded_model.predict([input_values])
     return jsonify({'prediction': str(prediction[0])})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
